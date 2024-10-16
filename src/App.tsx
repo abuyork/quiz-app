2b81cdc59567eee62e986b2ca24a5e3fe599126b
Abdugani <rashidovabdugan1@gmail.com>
2024-10-17 05:36:15 +0900

    Undo Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import HomePage from './components/HomePage'
 import Quiz from './components/Quiz'
